dd3b3e2805f4082ea6a14f93a5d3442a609cbb91
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-14 19:06:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,40 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useCallback, useState } from 'react'
+import SkillList from './SkillList'
 
+const App = () => {
+    const [input, setInput]= useState("");
+    const [skills, setSkills] =useState(["HTML","CSS","JavaScript", "React"]);
+    const addSkills= useCallback((e)=>{
+        e.preventDefault();
+        if(input && !skills.includes(input)){
+            setSkills([...skills, input]);
+
+        }
+        setInput("");
+    },[input,skills]);
+
+    const deleteSkill = useCallback(
+        (skill)=>{
+            setSkills(skills.filter((s)=>s!=skill));
+        },
+        [skills]
+    );
+
+  return (
+    <div>
+        <form onSubmit={addSkills}>
+          <h1 id='heading'>Skills List</h1>
+          <input 
+            id='skill-input'
+            value={input}
+            onChange={(e)=>setInput(e.target.value)}
+            />
+          <button id='skill-add-btn'>Add Skill</button>
+        </form>
+
+        <SkillList skills={skills} deleteSkill={deleteSkill}/>
+    </div>
+  )
+}
+
+export default App
